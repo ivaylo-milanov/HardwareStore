498d1f47caed429c704745150c5dc25d4b8a2e6c
ivaylo-milanov <imilanov24@gmail.com>
2023-11-19 18:15:53 +0200

    Added product controller

---
+++ HardwareStore.Web.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿namespace HardwareStore.Web.WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
